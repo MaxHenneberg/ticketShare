eb5c39042555d07781e69c6f87e340fae6e0080a
Fahad <14bscsmrana@seecs.edu.pk>
2020-06-02 20:08:26 +0200

    Added the database connection to the app
    	- Created a config folder with the keys
    	- Create a mongoose connection to our online db

--- code_base/backend/app.js
+++ code_base/backend/app.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+// db config get from config folder
+var db = require("./config/keys").MONGO_URL;
+
+// Connect to mongo
+mongoose
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("MongoDB Connected successfully!"))
+	.catch((err) => console.log(err));
+
+// body parser:
+app.use(express.urlencoded({ extended: false }));
+
 // routes
 app.use("/", require("./src/routes/routes"));
 
---
+++ code_base/backend/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	MONGO_URL:
+		"mongodb+srv://seba-user:bCufNxJDoGO3otuL@seba-aty2d.mongodb.net/Seba?retryWrites=true&w=majority",
+};
