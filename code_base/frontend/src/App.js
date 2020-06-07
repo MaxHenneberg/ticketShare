8e7005adfbd339aa2406575fb99b148ac1b060cb
NadaChatti <nadachatti95@gmail.com>
2020-06-08 01:57:39 +0200

    initial setup of frontend #12

--- code_base/frontend/src/App.js
+++ code_base/frontend/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+"use strict";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import { MovieListView } from './views/MovieListView';
+import { MovieDetailView }   from './views/MovieDetailView';
+import { MovieFormView }   from './views/MovieFormView';
+import { UserLoginView } from "./views/UserLoginView";
+import { UserSignupView } from "./views/UserSignupView";
+import UserService from "./services/UserService";
+
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: 'Movie Example App',
+            routes: [
+                { component: MovieListView , path: '/', exact: true},
+                { component: MovieDetailView , path: '/show/:id'},
+                { render: (props) => {
+                        if(UserService.isAuthenticated()) {
+                            return (<MovieFormView {... props} />)
+                        }
+                        else {
+                            return (<Redirect to={'/login'}/>)
+                        }} , path: '/edit/:id'},
+                { render: (props) => {
+                    if(UserService.isAuthenticated()) {
+                        return (<MovieFormView {... props} />)
+                    }
+                    else {
+                        return (<Redirect to={'/login'}/>)
+                    }}, path: '/add',},
+                { component: UserLoginView, path: '/login'},
+                { component: UserSignupView, path: '/register'}
+            ]
+        };
+    }
+
+    componentDidMount(){
+        document.title = this.state.title;
+    }
+
+    render() {
+        return(
+            <div>
+                <Router>
+                    <Switch>
+                        {this.state.routes.map((route, i) => (<Route key={i} {...route}/>) )}
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
-export default App;
