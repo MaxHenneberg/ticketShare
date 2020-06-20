19583f5ef1b15bd589751e2eae5c2086aca59308
Fahad <14bscsmrana@seecs.edu.pk>
2020-06-20 21:21:03 +0200

    Added boiler-plate code for  Group controller

---
+++ code_base/backend/src/controllers/group_controller.js
@@ -0,0 +1,10 @@
+const User = require("../models/user/user");
+const Group = require("../models/group/group");
+const Event = require("../models/group/eventInformation");
+const Join_Info = require("../models/group/joinInformation");
+const Ticket = require("../models/group/ticketInformation");
+const Search_Tag = require("../models/group/searchTag");
+
+exports.create = function (req, res) {
+
+};
