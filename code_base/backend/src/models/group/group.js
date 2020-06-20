45b44e24bdcdc17039670f033037aec4215b0d82
Fahad <14bscsmrana@seecs.edu.pk>
2020-06-20 21:29:01 +0200

    Fix the data-models and imports 	modified:   backend/src/models/chat/groupNotification.js 	modified:   backend/src/models/group/group.js 	modified:   backend/src/models/group/ticketInformation.js

--- code_base/backend/src/models/group/group.js
+++ code_base/backend/src/models/group/group.js
@@ -1,26 +1,40 @@
 const mongoose = require("mongoose");
-const JoinInformation = require("joinInformation");
+const JoinInformation = require("./joinInformation");
 const Price = require("../util/price");
-const GroupNotification = require("../chat/groupNotification")
+const GroupNotification = require("../chat/groupNotification");
 
-const GroupSchema = new mongoose.Schema({
-      type: String,
-      name: String,
-      desc: String,
+const GroupSchema = new mongoose.Schema(
+	{
+		type: String,
+		name: String,
+		desc: String,
 
-      //For now only the Path because of limited Resources on DB
-      // Maybe GridFS(https://docs.mongodb.com/manual/core/gridfs/) later
-      headerPic: String,
+		//For now only the Path because of limited Resources on DB
+		// Maybe GridFS(https://docs.mongodb.com/manual/core/gridfs/) later
+		headerPic: String,
 
-      public: Boolean,
-      pricePerPerson: Price,
-      joinDeadline: Date,
+		public: Boolean,
 
-      participants:[mongoose.Types.ObjectId],
+		pricePerPerson: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Price",
+		},
 
-      creator: mongoose.Types.ObjectId
-    },
-    {timestamps: true}
+		joinDeadline: Date,
+
+		participants: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+
+		creator: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("group", GroupSchema);
--- code_base/backend/src/models/group/ticketInformation.js
+++ code_base/backend/src/models/group/ticketInformation.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const Price = require("../util/price");
-const EventInformation = require("eventInformation")
+const EventInformation = require("./eventInformation")
 
 const TicketInformationSchema = new mongoose.Schema({
-      fullPrice: Price,
+      fullPrice: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Price",
+      },
       maxCoveredPeople: Number,
       initialFreeSlotsLeft: Number,
 
