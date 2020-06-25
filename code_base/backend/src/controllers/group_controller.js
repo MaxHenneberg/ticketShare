3c28d8f5cd2112f668e139fa17c6a258babfff79
Fahad <14bscsmrana@seecs.edu.pk>
2020-06-25 13:57:44 +0200

    Modified group controller to account for other documents/models
    	- Does neccesary checks for request validity
    	- Creates the models neccesary and then the group

--- code_base/backend/src/controllers/group_controller.js
+++ code_base/backend/src/controllers/group_controller.js
@@ -1,31 +1,84 @@
+"use strict";
+
 const User = require("../models/user/user");
 const Group = require("../models/group/group");
-const Event = require("../models/group/eventInformation");
-const Join_Info = require("../models/group/joinInformation");
+const EventInformation = require("../models/group/eventInformation");
 const Ticket = require("../models/group/ticketInformation");
+const Currency = require("../models/group/currency");
+const Join_Info = require("../models/group/joinInformation");
 const Search_Tag = require("../models/group/searchTag");
-const validator = require("validator");
 
-exports.create = function (req, res) {
+exports.create = async (req, res) => {
 	// TODO: Header Pic and Price Per Person
-	var group = null;
+	var validation = null;
+	var errors = [];
 	try {
+		var is_all = true;
 		validation = has_all_params(req.body, [
 			"name",
 			"type",
 			"desc",
 			"is_public",
+			"eventInformation",
+			"ticketInfo",
 		]);
 		if (validation.is_valid == false) {
 			throw validation.errors;
 		}
+		// validation of EventInformation
+		validation = has_all_params(req.body.eventInformation, [
+			"name",
+			"desc",
+			"eventStart",
+			"eventEnd",
+			"linkToEvent",
+		]);
+		if (validation.is_valid == false) {
+			is_all = false;
+			errors = errors.concat({ eventInformation: validation.errors });
+		}
+		// validation of TicketInformation
+		validation = has_all_params(req.body.ticketInfo, [
+			"fullPrice",
+			"maxCoveredPeople",
+			"initialFreeSlotsLeft",
+			"currency",
+		]);
+		if (validation.is_valid == false) {
+			is_all = false;
+			errors = errors.concat({ ticketInfo: validation.errors });
+		}
+		if (!is_all) throw errors;
+	} catch (err) {
+		return res.status(400).send({
+			errors: err,
+		});
+	}
+
+	try {
+		// validate if currency exists in db
+		var currency_id = req.body.ticketInfo.currency;
+		if (!currency_id.match(/^[0-9a-fA-F]{24}$/)) {
+			throw "Invalid Currency ID format";
+		}
+		let currency_object = await Currency.findById(currency_id).exec();
+		if (!currency_object) throw "Invalid Currency";
+		/*
+			Create documents.
+		*/
+		// create EventInformation
+		let event_info = await EventInformation.create(req.body.eventInformation);
+		// create ticket info
+		req.body.ticketInfo.eventInformation = event_info.id;
+		let ticket = await Ticket.create(req.body.ticketInfo);
 		// create group
-		group = new Group({
+		let group = await Group.create({
 			name: req.body.name,
 			type: req.body.type,
 			desc: req.body.desc,
 			public: req.body.is_public,
 			creator: req.user.id,
+			event: event_info.id,
 		});
 		return res.status(200).send({ group: group.id });
 	} catch (err) {
