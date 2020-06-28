83d3faac6b1142c73a13fdf3b3b8a393a967f130
Fahad <14bscsmrana@seecs.edu.pk>
2020-06-28 17:14:21 +0200

    Changed models according to requirements and needs
    Closes #36
    Closes #37

--- code_base/backend/src/models/group/group.js
+++ code_base/backend/src/models/group/group.js
@@ -1,9 +1,6 @@
 "use strict";
 
 const mongoose = require("mongoose");
-const JoinInformation = require("./joinInformation");
-const Price = require("../util/price");
-const GroupNotification = require("../chat/groupNotification");
 
 const GroupSchema = new mongoose.Schema(
 	{
@@ -14,13 +11,13 @@ const GroupSchema = new mongoose.Schema(
 		//For now only the Path because of limited Resources on DB
 		// Maybe GridFS(https://docs.mongodb.com/manual/core/gridfs/) later
 		headerPic: String,
-
+		joinDeadline: Date,
 		public: Boolean,
 
-		// event 
-		event: {
+		// ticket
+		ticket: {
 			type: mongoose.Schema.Types.ObjectId,
-			ref: "EventInformation",
+			ref: "TicketInformation",
 		},
 		//user
 		creator: {
