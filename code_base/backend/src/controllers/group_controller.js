c32c29ba84bb1e047a565b5d6b75bc73e9dda421
Fahad <14bscsmrana@seecs.edu.pk>
2020-06-28 22:05:33 +0200

    Added more checks for Sanity.. See below
    	- Dates comparison. Start must be  <  End date
    	- Free slots must be less than total capacity
    	- Added validator for Join Deadline and must be greater or equal to today

--- code_base/backend/src/controllers/group_controller.js
+++ code_base/backend/src/controllers/group_controller.js
@@ -22,6 +22,12 @@ exports.validate = (method) => {
 				body("name", "Group Name Required").exists().notEmpty(),
 				body("type", "Group Type Required").exists().notEmpty(),
 				body("desc", "Group Description Required").exists().notEmpty(),
+				body("joinDeadline", "Join Deadline Required")
+					.exists()
+					.notEmpty()
+					.bail()
+					.isDate()
+					.withMessage("Invalid Join Date Format"),
 				body("is_public", "Public Parameter Required")
 					.exists()
 					.isBoolean()
@@ -39,7 +45,7 @@ exports.validate = (method) => {
 					.notEmpty()
 					.bail()
 					.isInt({ gt: 1 })
-					.withMessage("Invalid value for Max People"),
+					.withMessage("Invalid value for Max People (Must be greater than 1)"),
 				body("ticketInfo.initialFreeSlotsLeft", "Ticket Free Slots Required")
 					.exists()
 					.notEmpty()
@@ -53,9 +59,15 @@ exports.validate = (method) => {
 					.isMongoId()
 					.withMessage("Invalid Value for Currency"),
 				// validate event info
-				body("eventInformation", "Event Information Required").exists().notEmpty(),
-				body("eventInformation.name", "Event Name Required").exists().notEmpty(),
-				body("eventInformation.desc", "Event Description Required").exists().notEmpty(),
+				body("eventInformation", "Event Information Required")
+					.exists()
+					.notEmpty(),
+				body("eventInformation.name", "Event Name Required")
+					.exists()
+					.notEmpty(),
+				body("eventInformation.desc", "Event Description Required")
+					.exists()
+					.notEmpty(),
 				body("eventInformation.eventStart", "Event Start Date Required")
 					.exists()
 					.notEmpty()
@@ -83,16 +95,31 @@ exports.validate = (method) => {
 
 exports.create = async (req, res) => {
 	// TODO: Header Pic and Price Per Person
+	// let c = await Currency.create({name:'Euro',short_form:'eur',symbol:"€"}).exec();
+	// return;
+
 	const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+	if (!errors.isEmpty()) {
+		return res.status(400).json(errors);
+	}
 	try {
-		if (!errors.isEmpty()) {
-			throw errors;
-		}
+		var start_date = new Date(req.body.eventInformation.eventStart);
+		var end_date = new Date(req.body.eventInformation.eventEnd);
+		var joinDeadline = new Date(req.body.joinDeadline).setHours(0, 0, 0, 0);
+		var today = new Date().setHours(0, 0, 0, 0);
+		if (today > joinDeadline) throw "Deadline can not be for a past date";
+		if (start_date > end_date) throw "Event Start Date can't be after End Date";
 
 		var currency_id = req.body.ticketInfo.currency;
 		let currency_object = await Currency.findById(currency_id).exec();
-		if (!currency_object) throw { msg: "Invalid Currency" };
+		if (!currency_object) throw "Invalid Currency";
 
+		if (
+			req.body.ticketInfo.initialFreeSlotsLeft >=
+			req.body.ticketInfo.maxCoveredPeople
+		) {
+			throw "Free Slots must be less than Max Covered People. We include you as one ;)";
+		}
 		/*
 			Create documents.
 		*/
@@ -103,23 +130,22 @@ exports.create = async (req, res) => {
 		req.body.ticketInfo.eventInformation = event_info.id;
 		let ticket = await Ticket.create(req.body.ticketInfo);
 
+		var creator_id = req.user.id;
+
 		// create group
-		if(req.user){
-			var creator_id = req.user.id
-		}
-		else{
-			var creator_id = '5ef48f419e78460424b2c250'
-		}
 		let group = await Group.create({
 			name: req.body.name,
 			type: req.body.type,
 			desc: req.body.desc,
+			joinDeadline: req.body.joinDeadline,
 			public: req.body.is_public,
 			creator: creator_id,
-			event: event_info.id,
+			ticket: ticket.id,
 		});
+		console.log(group);
 		return res.status(200).json({ group: group.id });
 	} catch (err) {
+		err = { errors: [{ msg: err }] };
 		return res.status(400).json(err);
 	}
 };
