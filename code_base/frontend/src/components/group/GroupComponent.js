b25cb06e793c9187ce992000b0d839c5d3fa7203
Fahad <14bscsmrana@seecs.edu.pk>
2020-07-04 20:28:38 +0200

    Added state to group component and bind data to the fetch

--- code_base/frontend/src/components/group/GroupComponent.js
+++ code_base/frontend/src/components/group/GroupComponent.js
@@ -9,7 +9,7 @@ import Card from "react-bootstrap/Card";
 class GroupComponent extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = { ticket: { currency: {} } };
 	}
 	componentDidMount() {
 		fetch("http://localhost:8080/group/" + this.props.group_id)
@@ -18,27 +18,46 @@ class GroupComponent extends React.Component {
 			})
 			.then((data) => {
 				this.setState(data);
+				this.populatePricePerPerson();
+				this.convertDate();
 				console.log(this.state);
 			})
 			.catch((error) => {
 				console.log(error);
 			});
 	}
+	populatePricePerPerson() {
+		var price_per_person =
+			this.state.ticket.fullPrice / this.state.ticket.maxCoveredPeople;
+		this.setState({ price_per_person: price_per_person });
+		return;
+	}
+	convertDate() {
+		var d = new Date(this.state.joinDeadline).toDateString();
+		this.setState({ joinDeadline: d });
+		return;
+	}
 	render() {
 		return (
 			<Row>
 				<Col>
 					<Card>
-						<Card.Title style={{ paddingTop: "10px" }}>
-							{this.state.name}
-						</Card.Title>
-						<hr></hr>
+						<h2 style={{ paddingTop: "10px" }}>{this.state.name}</h2>
 						<Card.Body>
 							<Col>
-							
-							</Col>
-							<Col>
-								Temp 3 places remain
+								<Row>
+									<Col>
+										Price per Person:
+										<h3>
+											{this.state.ticket.currency.symbol}{" "}
+											{this.state.price_per_person}
+										</h3>
+									</Col>
+									<Col>
+										Join Last Date: <h3>{this.state.joinDeadline}</h3>
+									</Col>
+									<Col>TODO. places remaining</Col>
+								</Row>
 							</Col>
 						</Card.Body>
 					</Card>
