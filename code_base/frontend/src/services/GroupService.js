b14bf4b74a2d4cb4c1733af2f89433f4eee98cff
Fahad <14bscsmrana@seecs.edu.pk>
2020-07-04 22:05:12 +0200

    created groupservice to use in code based on HTTPService

---
+++ code_base/frontend/src/services/GroupService.js
@@ -0,0 +1,27 @@
+import HttpService from './HttpService';
+
+export default class GroupService {
+
+    constructor(){
+    }
+
+    static baseURL() {return 'http://localhost:8080/group/' }
+
+    static getGroup(id) {
+        return new Promise((resolve, reject) => {
+            var url = GroupService.baseURL()+id;
+            console.log(url);
+            HttpService.get(url, function(data) {
+                if(data != undefined || Object.keys(data).length !== 0) {
+                    resolve(data);
+                }
+                else {
+                    reject('Error while retrieving group');
+                }
+            }, function(textStatus) {
+                reject(textStatus);
+            });
+        });
+    }
+
+}
