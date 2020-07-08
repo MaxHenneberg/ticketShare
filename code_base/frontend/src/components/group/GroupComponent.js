a8f1b5cac84af981f79f4fd3035b33982e574f19
Fahad <14bscsmrana@seecs.edu.pk>
2020-07-08 22:36:21 +0200

    Change the group component to have a collapse with more details and join button

--- code_base/frontend/src/components/group/GroupComponent.js
+++ code_base/frontend/src/components/group/GroupComponent.js
@@ -5,7 +5,11 @@ import { withRouter } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+import Fade from "react-bootstrap/Fade";
 import GroupService from "../../services/GroupService";
+import { AlignCenter } from "react-bootstrap-icons";
 
 class GroupComponent extends React.Component {
 	constructor(props) {
@@ -26,13 +30,27 @@ class GroupComponent extends React.Component {
 					_id: "",
 					symbol: "Loading...",
 				},
+				eventInformation: {
+					_id: "Loading...",
+					name: "Loading...",
+					desc: "Loading...",
+					eventStart: "Loading...",
+					eventEnd: "Loading...",
+					linkToEvent: "",
+					createdAt: "Loading...",
+					updatedAt: "Loading...",
+					__v: 0,
+				},
 			},
-			emptySlots: "Loading.."
+			emptySlots: "Loading..",
+			eventStart: "Loading",
+			eventEnd: "Loading",
+			collapsed: false,
 		};
+		this.changeCollapse = this.changeCollapse_fn.bind(this);
 	}
 	componentDidMount() {
 		let id = this.props.id;
-
 		(async () => {
 			try {
 				let group = await GroupService.getGroup(id);
@@ -46,14 +64,19 @@ class GroupComponent extends React.Component {
 		})();
 	}
 	populatePricePerPerson() {
-		var price_per_person =
-			this.state.ticket.fullPrice / this.state.ticket.maxCoveredPeople;
+		var price_per_person = this.state.ticket.fullPrice / this.state.ticket.maxCoveredPeople;
 		this.setState({ price_per_person: price_per_person });
 		return;
 	}
 	convertDate() {
-		var d = new Date(this.state.joinDeadline).toDateString();
-		this.setState({ joinDeadline: d });
+		var d = new Date(this.state.joinDeadline).toDateString().split(" ").slice(1).join(" ");
+		var d2 = new Date(this.state.ticket.eventInformation.eventStart).toDateString().split(" ").slice(1).join(" ");
+		var d3 = new Date(this.state.ticket.eventInformation.eventEnd).toDateString().split(" ").slice(1).join(" ");
+		this.setState({
+			joinDeadline: d,
+			eventStart: d2,
+			eventEnd: d3,
+		});
 		return;
 	}
 	async getEmptySlots(id) {
@@ -61,35 +84,93 @@ class GroupComponent extends React.Component {
 		this.setState({ emptySlots: response.free_slots });
 		return;
 	}
+	changeCollapse_fn() {
+		let current = this.state.collapsed;
+		this.setState({ collapsed: !current });
+	}
 	render() {
 		return (
 			<Row>
 				<Col>
 					<Card>
-						<h2 style={{ paddingTop: "10px" }}>{this.state.name}</h2>
-						<Card.Body>
+						<Row style={{ paddingTop: "10px" }}>
+							<Col xs={8}>
+								<center>
+									<h2><b>{this.state.name}</b></h2>
+								</center>
+							</Col>
 							<Col>
-								<Row>
-									<Col>
+								<center>
+									<Button
+										variant="warning"
+										onClick={this.changeCollapse}
+										aria-controls={"collapse_" + this.props.id}
+										aria-expanded={this.state.collapsed}
+									>
+										Show Details
+									</Button>
+								</center>
+							</Col>
+						</Row>
+						<Card.Body>
+							<Row>
+								<Col>
+									<center>
 										Price per Person:
 										<h3>
-											{this.state.ticket.currency.symbol}{" "}
-											{this.state.price_per_person}
+											{this.state.ticket.currency.symbol} {this.state.price_per_person}
 										</h3>
-									</Col>
-									<Col>
-										Join Last Date: <h3>{this.state.joinDeadline}</h3>
-									</Col>
-									<Col>
+									</center>
+								</Col>
+								<Col>
+									<center>
+										Join Last Date:
+										<h3>{this.state.joinDeadline}</h3>
+									</center>
+								</Col>
+								<Col>
+									<center>
 										Places Remaining:{" "}
 										<h3>
 											{" "}
-											{this.state.emptySlots}{" "}/{" "}
-											{this.state.ticket.maxCoveredPeople}
+											{this.state.emptySlots} / {this.state.ticket.maxCoveredPeople}
 										</h3>
-									</Col>
-								</Row>
-							</Col>
+									</center>
+								</Col>
+							</Row>
+							<br></br>
+							<Row>
+								<Col>
+									<Collapse in={this.state.collapsed}>
+										<div id={"collapse_" + this.props.id}>
+											<Row>
+												<Col>
+													<center>
+														Event Name: <h3> {this.state.ticket.eventInformation.name}</h3>
+													</center>
+												</Col>
+
+												<Col>
+													<center>
+														Event Dates:{" "}
+														<h3>
+															{" "}
+															{this.state.eventStart}
+															{" - "}
+															{this.state.eventEnd}
+														</h3>
+													</center>
+												</Col>
+												<Col>
+													<center>
+														<Button variant="success">+ Join Group</Button>
+													</center>
+												</Col>
+											</Row>
+										</div>
+									</Collapse>
+								</Col>
+							</Row>
 						</Card.Body>
 					</Card>
 				</Col>
